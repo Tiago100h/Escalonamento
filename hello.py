510563cd2eec191df5dac253fbe24e97a011b0b3
Tiago Giuliatte Pereira <tiagogiuliatte.p@gmail.com>
2018-08-15 20:23:44 -0300

    Lendo o arquivo e obtendo  a maior linha

--- hello.py
+++ hello.py
@@ -1,2 +1,13 @@
-msg = "Hello World"
-print(msg)
+# Lendo arquivo
+arquivo = open('arquivos/teste.txt', 'r')
+linhas = arquivo.readlines()
+
+maiorLinha = 0
+for linha in linhas :
+    elementos = linha.split(' ')
+    if len(elementos) > maiorLinha :
+        maiorLinha = len(elementos)
+
+print(maiorLinha)
+
+arquivo.close()
