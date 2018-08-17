3c3fc30ebce640480b0ed7d2afe0fb01b5e252eb
Tiago Giuliatte Pereira <tiagogiuliatte.p@gmail.com>
2018-08-17 20:15:46 -0300

    Criando matriz de inteiro com zeros a esquerda. Criando função para imprimir matriz.

--- hello.py
+++ hello.py
@@ -1,13 +1,39 @@
-# Lendo arquivo
+# Função para imprimir matriz
+def ImprimirMatriz(matriz):
+    i = 0
+    j = 0
+    while i < len(matriz):
+        while j < len(matriz[i]):
+            print(matriz[i][j], end=' ')
+            j = j + 1
+        print()
+        i = i + 1
+        j = 0
+    print()
+
+# Ler arquivo
 arquivo = open('arquivos/teste.txt', 'r')
 linhas = arquivo.readlines()
+arquivo.close()
 
+# Definir maior linha
 maiorLinha = 0
-for linha in linhas :
+for linha in linhas:
     elementos = linha.split(' ')
-    if len(elementos) > maiorLinha :
+    if len(elementos) > maiorLinha:
         maiorLinha = len(elementos)
 
-print(maiorLinha)
+# Criar matriz com inteiros e zeros a esquerda
+matriz = []
+for linha in linhas:
+    elementos = linha.split(' ')
+    novaLinha = []
+    i = len(elementos)
+    while i < maiorLinha:
+        novaLinha.append(0)
+        i = i + 1
+    for elemento in elementos:
+        novaLinha.append(int(elemento.replace('\n', '')))
+    matriz.append(novaLinha)
 
-arquivo.close()
+ImprimirMatriz(matriz)
